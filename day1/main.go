fc49cc2271c4b0dd38bfcc8b583dc8cf76887f79
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-01-16 23:21:09 -0300

    day 1.0 resolution

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("day 1")
+	res := trebuchet()
+	fmt.Println(res)
+}
+
+func trebuchet() int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	targets := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		limit := len(line) - 1
+	first:
+		for _, c := range line {
+			for _, t := range targets {
+				if t == string(c) {
+					if d, err := strconv.Atoi(t); err == nil {
+						total += 10 * d
+						break first
+					}
+				}
+			}
+		}
+	last:
+		for c := limit; c >= 0; c-- {
+			for _, t := range targets {
+				if t == string(line[c]) {
+					if d, err := strconv.Atoi(t); err == nil {
+						total += d
+						break last
+					}
+				}
+			}
+		}
+	}
+	return total
 }
